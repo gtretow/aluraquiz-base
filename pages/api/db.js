bc462fc077c0fc8a9695a9e1108aea514252fec0
gtretow <guilherme.tretow@gmail.com>
2021-01-28 20:12:55 -0300

    api/db done

---
+++ pages/api/db.js
@@ -0,0 +1,20 @@
+/* eslint-disable comma-dangle */
+/* eslint-disable quotes */
+/* eslint-disable func-names */
+import db from "../../db.json";
+
+export default function (req, res) {
+  if (res.method === "OPTIONS") {
+    res.status(200).end();
+    return;
+  }
+
+  res.setHeader("Access-Control-Allow-Credentials", true);
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader(
+    "Access-Control-Allow-Methods",
+    "GET, OPTIONS, PATCH, DELETE, POST, PUT"
+  );
+
+  res.json(db);
+}
---
+++ pages/quiz/[id].js
@@ -0,0 +1,6 @@
+/* eslint-disable quotes */
+import React from "react";
+
+export default function QuizDaGalerapage() {
+  return <div>Desafio futuro</div>;
+}
